2e44c20c074a1ed46330f904cd8f6d39a3d9a4c5
andreitoledo <andreiltoledo@hotmail.com>
2023-06-19 23:16:38 -0300

    Criando e configurando a classe VooMap

---
+++ src/Andreitoledo.UoW.Data/Mappings/VooMap.cs
@@ -0,0 +1,33 @@
+﻿using Andreitoledo.UoW.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Andreitoledo.UoW.Data.Mappings
+{
+    public class VooMap : IEntityTypeConfiguration<Voo>
+    {
+        public void Configure(EntityTypeBuilder<Voo> builder)
+        {
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Codigo)
+                .IsRequired()
+                .HasMaxLength(10)
+                .HasColumnType("varchar");
+
+            builder.Property(x => x.Nota)
+                .HasMaxLength(100)
+                .IsRequired()
+                .HasColumnType("varchar(100)");
+
+            // Relationship
+            // um voo para muitas pessoas
+            // pessoas para um voo
+            builder.HasMany(x => x.Pessoas).WithOne(x => x.Voo).HasForeignKey(fk => fk.VooId);
+
+
+
+
+        }
+    }
+}
