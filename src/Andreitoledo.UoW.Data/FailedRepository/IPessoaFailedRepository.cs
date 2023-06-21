79efd50e242ba2f14d06d04c436e41fc10e78af3
andreitoledo <andreiltoledo@hotmail.com>
2023-06-21 14:11:08 -0300

    Criando a Interface e a Classe Concreta de IPessoaFailedRepository

---
+++ src/Andreitoledo.UoW.Data/FailedRepository/IPessoaFailedRepository.cs
@@ -0,0 +1,27 @@
+﻿using Andreitoledo.UoW.Data.FailedRepository;
+using Andreitoledo.UoW.Data.Orm;
+using Andreitoledo.UoW.Domain;
+
+namespace Andreitoledo.UoW.Data.FailedRepository
+{
+    public interface IPessoaFailedRepository
+    {
+        Task AdicionarSeAoVoo(Pessoa pessoa);
+    }
+}
+
+public partial class PessoaFailedRepository : IPessoaFailedRepository
+{
+    private readonly UoWDbContext _context;
+
+    public PessoaFailedRepository(UoWDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task AdicionarSeAoVoo(Pessoa pessoa)
+    {
+        await _context.Set<Pessoa>().AddAsync(pessoa);
+        await _context.SaveChangesAsync();
+    }
+}
