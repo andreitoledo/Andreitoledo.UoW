b8edc239cf56a1c650909979f313c1353c7c1158
andreitoledo <andreiltoledo@hotmail.com>
2023-06-21 20:04:04 -0300

    Criando PessoaDTO, PessoaRequest e PessoaFailedController - Parte I

---
+++ src/Andreitoledo.UoW.Api/Controllers/PessoaFailedController.cs
@@ -0,0 +1,23 @@
+﻿using Andreitoledo.UoW.Data.FailedRepository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Andreitoledo.UoW.Api.Controllers
+{
+    [ApiController]
+    [Route("api/Controller")]
+    public class PessoaFailedController : Controller
+    {
+        private readonly IPessoaFailedRepository _repo;
+
+        public PessoaFailedController(IPessoaFailedRepository repo)
+        {
+            _repo = repo;
+        }
+
+        [HttpGet("pagina-inicial")]
+        public IActionResult Index()
+        {
+            return Ok();
+        }
+    }
+}
