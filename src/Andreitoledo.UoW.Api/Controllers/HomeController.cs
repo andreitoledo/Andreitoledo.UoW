9c2830986b05cd0f7ee2c2dfc6446fd1c2a8408d
andreitoledo <andreiltoledo@hotmail.com>
2023-06-21 18:21:52 -0300

    Criando e configurando a Startup Class e a primeira Controller

---
+++ src/Andreitoledo.UoW.Api/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Andreitoledo.UoW.Api.Controllers
+{
+    [ApiController]
+    [Route("api/pagina-inicial")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Ok();
+        }
+    }
+}
--- src/Andreitoledo.UoW.Api/Startup.cs
+++ src/Andreitoledo.UoW.Api/Startup.cs
@@ -2,5 +2,35 @@
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public void Configureservices(IServiceCollection services)
+        {
+            services.AddControllers();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env) 
+        { 
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseRouting();
+            app.UseAuthentication();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        
+        }
+
+
     }
 }
