e2b1b54fd62a64d0bbd80b7b720073d44a42c722
andreitoledo <andreiltoledo@hotmail.com>
2023-06-23 15:28:35 -0300

    Criando PessoaController e utilizando UoW

---
+++ src/Andreitoledo.UoW.Api/Controllers/PessoaController.cs
@@ -0,0 +1,59 @@
+﻿using Andreitoledo.UoW.Api.Models;
+using Andreitoledo.UoW.Data.FailedRepository;
+using Andreitoledo.UoW.Data.repositories.Abstraction;
+using Andreitoledo.UoW.Domain;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Andreitoledo.UoW.Api.Controllers
+{
+    [ApiController]
+    [Route("api/pessoa")]
+    public class PessoaController : Controller
+    {
+        private readonly IPessoaRepository _repoPessoa;
+        private readonly IVooRepository _repoVoo;
+        private readonly IMapper _mapper;
+
+        public PessoaController(IPessoaRepository repoPessoa,
+                                      IVooRepository repoVoo,
+                                      IMapper mapper)
+        {
+            _repoPessoa = repoPessoa;
+            _repoVoo = repoVoo;
+            _mapper = mapper;
+        }
+
+        [HttpPost("adicionar-passageiro")]
+        [ProducesResponseType(typeof(PessoaDTO), StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> AdicionarPassageiro(PessoaRequest pessoa)
+        {
+            if (!ModelState.IsValid) return BadRequest("O modelo está inválido");
+
+            var pessoaModel = new Pessoa
+            {
+                VooId = pessoa.VooId,
+                Nome = pessoa.Nome                
+            };
+
+            try
+            {
+                await _repoPessoa.AdicionarSeAoVoo(pessoaModel);
+                await _repoVoo.DecrementarVaga(pessoa.VooId);
+
+                var transacao = await _repoPessoa.Commit();
+
+                return CreatedAtAction(nameof(AdicionarPassageiro),
+                    _mapper.Map<PessoaDTO>(pessoaModel));
+
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+        }
+
+    }
+}
