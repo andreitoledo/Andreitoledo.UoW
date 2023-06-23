890e04de07249092f38eb946104aea55f1175ad9
andreitoledo <andreiltoledo@hotmail.com>
2023-06-22 23:53:05 -0300

    Criando IPessoaRepository, IUniOfWork, IVooRepository reais

---
+++ src/Andreitoledo.UoW.Data/repositories/Abstraction/IPessoaRepository.cs
@@ -0,0 +1,15 @@
+﻿using Andreitoledo.UoW.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Andreitoledo.UoW.Data.repositories.Abstraction
+{
+    public interface IPessoaRepository : IUnitOfWork
+    {
+        Task AdicionarSeAoVoo(Pessoa pessoa);
+        Task ExcluirPessoaDoVoo(Guid vooId);
+    }
+}
---
+++ src/Andreitoledo.UoW.Data/repositories/Abstraction/IVooRepository.cs
@@ -0,0 +1,20 @@
+﻿using Andreitoledo.UoW.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Andreitoledo.UoW.Data.repositories.Abstraction
+{
+    public interface IVooRepository : IUnitOfWork
+    {
+        Task DecrementarVaga(Guid? vooId);
+        Task UpdateVoo(Voo voo);
+        Task<Voo> SelecionarPorId(Guid? id);
+        Task<IEnumerable<Voo>> SelecionarTodos(Expression<Func<Voo, bool>> quando = null);
+        Task Criar(Voo voo);
+
+    }
+}
